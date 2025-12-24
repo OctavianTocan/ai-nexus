d94d4e7212d5bdb66972dc5e07bbdbf79ec03be1
Octavian Tocan <octavian.tocan@thirdear.ai>
2025-12-25 00:08:27 +0100

    feat: Add prompt input composer to chat, configure Next.js scripts for port 3001, and mark page as client.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Chat from "@/components/chat/chat";
 
 export default function Page() {
--- frontend/components/chat/chat.tsx
+++ frontend/components/chat/chat.tsx
@@ -5,10 +5,11 @@ import {
     MessageResponse,
 } from '@/components/ai-elements/message';
 import { Conversation } from '../ai-elements/conversation';
+import { PromptInput, PromptInputAttachment, PromptInputAttachments, PromptInputBody, PromptInputButton, PromptInputFooter, PromptInputHeader, PromptInputSubmit, PromptInputTextarea } from '../ai-elements/prompt-input';
 const Chat = () => {
     return (
         <>
-            <div className='w-full h-full'>
+            <div className='size-full'>
                 <div className='w-[40%] mt-[5%] mx-auto'>
                     <Conversation children={
                         <>
@@ -24,8 +25,25 @@ const Chat = () => {
                             </Message>
                         </>
                     }>
-
                     </Conversation>
+                    {/* Composer */}
+                    <PromptInput onSubmit={(message) => console.log(message)} className='mt-[5%]'>
+                        {/* Header */}
+                        {/* <PromptInputHeader> */}
+                        {/* Attachments */}
+                        {/* <PromptInputAttachments> */}
+                        {/* {
+                                    (attachment) => <PromptInputAttachment data={attachment} />
+                                } */}
+                        {/* </PromptInputAttachments> */}
+                        {/* </PromptInputHeader> */}
+
+                        {/* Body */}
+                        <PromptInputBody className=''>
+                            <PromptInputTextarea placeholder='Ask something...' className='pr-12 min-h-[60px]' />
+                            <PromptInputSubmit className='absolute bottom-1 right-1'>Send</PromptInputSubmit>
+                        </PromptInputBody>
+                    </PromptInput>
                 </div>
             </div>
         </>
